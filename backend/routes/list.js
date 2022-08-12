67df3ffef44ce8d8fec4a6743fe2a58d3e7556f4
Pranjay91 <pranjaylohbare91@gmail.com>
2022-08-13 04:10:19 +0530

    backend done front end 3 page done

--- backend/routes/list.js
+++ backend/routes/list.js
@@ -1,43 +1,83 @@
-const express = require("express")
-const listModel = require("../models/list")
-const router = express.Router()
+const express = require("express");
 const jwt = require("jsonwebtoken")
-
-// router.get("/list", (req, res)=> {
-//     try{
-//         const username = jwt.verify(req.headers.authorization,process.env.SECRET_KEY)
-//         listModel.find({username:username}).then((listData)=> {
-//             res.status(200).send(listData); 
-//     }).catch((err)=>{
-//         res.status(400).send(err)
-//     })
-// }catch(err){
-//     res.status(403).send("User Not Authorized", err)
-// }
-// });
-
-router.get("/list",(req,res)=>{
-    listModel.find({}).then((listData)=>{
-    res.status(200).send(listData)
-     //console.log(listData)
-})
-})
-
-router.get("/",(req,res)=>{
-res.status(200).send("working")
-})
+const router = express.Router();
+const signupModal = require("../models/signup-Modal");
+const  listModel=require("../models/list");
 
 
-router.post("/add",(req,res)=>{
-listModel.create(req.body).then((data)=> {
-    res.status(200).send(data)
-}).catch((err)=>{
-    res.status(400).send(err)
+router.get("/", (req, res) => {
+
+    if (req.headers.authorization) {
+        try {
+            user_mail = jwt.verify(req.headers.authorization, process.env.SECRET_KEY);
+            signupModal.find({ email: user_mail }).then((userData) => {
+                if (userData.length) {
+                    // console.log(user_mail)
+                             listModel.find().then((propertyData) => {
+                               const value= propertyData.reverse()
+                        res.status(200).send(value)                
+                    })
+                } else {
+                    res.status(403).send("No such user exist with the mentioned email id")
+                   }
+            }).catch((err) => {
+                res.status(403).send(err.message)
+            })
+        } catch (err) {
+            res.status(500).send("User not authorized")
+        }
+    } else {
+        res.status(200).send("header is empty please add header")
+    }
 })
+
+
+router.get("/search/:id", (req, res) => {
+    listModel.find({_id:req.params.id}).then((propertyData) => {
+        // console.log(propertyData)
+        res.status(200).send(propertyData)
+    }).catch(err=>{
+        console.log("error occured")
+        res.status(400).send("no id matching with the entered id")
+    })
+
+
+
+    // if (req.headers.authorization) {
+
+    //     try {
+    //         user_mail = jwt.verify(req.headers.authorization, process.env.SECRET_KEY);
+    //         signupModal.find({ email: user_mail }).then((userData) => {
+
+    //             if (userData.length) {
+    //                 console.log(user_mail)
+                    
+    //                 PropertyDetailsModel.find({_id:req.params.id}).then((propertyData) => {
+    //                     res.status(200).send(propertyData)
+    //                 //   if(propertyData.length) {
+    //                 //      res.status(200).send(propertyData)
+    //                 //     }else{
+    //                 //     res.status(203).send("No such post exist")
+    //                 //    }
+    //                 })
+
+    //             } else {
+    //                 res.status(403).send("No such user exist with the mentioned email id")
+    //             }
+
+    //         }).catch((err) => {
+    //             res.status(403).send(err.message)
+    //         })
+    //     } catch (err) {
+    //         res.status(500).send("User not authorized")
+    //     }
+
+    // } else {
+    //     res.status(200).send("header is empty please add header")
+    // }
+
 })
+
+
 
 module.exports = router;
-
-
-
-
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,30 @@
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import List from './components/list/list';
+
+import Signup from "./components/Signup/Signup";
+import Signin from "./components/Signin/Signin";
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import './App.css';
+import PropertyView from './components/PropertyView';
+// import AddProperty from './components/AddProperty';
+//import AddProperty from "./components/addProperty/addProperty";
+import Logout from "./components/Logout/Logout";
+
 
 function App() {
   return (
-  <>
-     <BrowserRouter>
+    <div className="App">
+   
+      <BrowserRouter>
       <Routes>
-       <Route path="/list" element={<List/>}></Route>
-      </Routes>
-      
-     </BrowserRouter>
-  </>
+            <Route path="/" element={<Signin/>}></Route>
+             <Route path="/signup" element={<Signup/>}></Route> 
+            <Route path="/listproperty" element={<PropertyView/>}></Route>
+            {/* <Route path="/addproperty" element={<AddProperty/>}></Route> */}
+            <Route path="/logout"  element={<Logout/>}></Route> 
+          </Routes>
+      </BrowserRouter>
      
+    </div>
+
   );
 }
 
